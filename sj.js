7330ac5ace7ac229448f7778a1863a69cbfc21ff
angimeucci <accountstudente@mail.com>
2021-03-28 19:47:56 +0200

    2 - completata tutta la parte al di fuori del trattamento dei dati

--- sj.js
+++ sj.js
@@ -1,24 +1,61 @@
-function maindelete(elem) {
-    if (elem == null) {
-        var allelement = document.getElementsByName('checkbox');
-        document.getElementById('content').removeChild((row + allelement.length).parentNode);
-    } else {
-        document.getElementById('content').removeChild(elem.parentNode);
+//__________________global item______________________________________
+var elem = 0;
+
+class Employer {
+    constructor(name, email, address, phone) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+};
+
+//__________________main function____________________________________
+function loadallelement() {
+
+}
+
+function deletepls(element) {
+    try {
+        if (element == undefined) {
+            var checkboxes = document.getElementsByName('checkbox');
+            for (var i = 0, n = checkboxes.length; i < n; i++)
+                if (checkboxes[i].checked == true)
+                    document.getElementById('itemscontainer').removeChild(document.getElementById(checkboxes[i].id));
+        } else {
+            if (confirm("I\'m sure about what I\'m doing"))
+                document.getElementById('itemscontainer').removeChild(document.getElementById(element));
+        }
+    } catch (Exception) {
+        //I didn't understand why this happen
     }
 }
 
-function mainadd() {
-    var html = '<td><input class="form-check-input" type="checkbox" name="checkbox" style="margin-left: 1%"></td><td><p>Name</p></td><td><p>Email</p></td><td><p>Address</p></td><td><p>Phone</p></td>';
-    html = html + '<td><img onClick="modify()" src="media/modificabut.png" style="width: 30px; height: 30px;"></td>';
-    html = html + '<td><img onClick="modify()" src="media/removebut.png" style="width: 30px; height: 30px;"></td>';
+function mainadd(Employer) {
+    var html = '<td><input class="form-check-input" type="checkbox" name="checkbox" style="margin-left: 1%" id="' + elem + '"></td><td id="' + elem + '" name="name"><p>' + Employer.name + '</p></td><td id="' + elem + '" name="email"><p>' + Employer.email + '</p></td><td id="' + elem + '" name="address"><p>' + Employer.address + '</p></td><td id="' + elem + '" name="phone"><p>' + Employer.phone + '</p></td>';
+    html = html + '<td style="width: 1%"><img onClick="modify()" src="media/modificabut.png" style="width: 30px; height: 30px;" id="' + elem + '"></td>';
+    html = html + '<td><img onClick="deletepls(this.id)" src="media/removebut.png" style="width: 30px; height: 30px;" id="' + elem + '"></td>';
     const row = document.createElement('tr');
     var allelement = document.getElementsByName('checkbox');
-    row.id = (row + allelement.length);
+    row.id = (elem);
     row.innerHTML = html;
     document.getElementById('itemscontainer').appendChild(row);
+    elem++;
 }
 
-/*
+function checkboxall(maincheckbox) {
+    var checkboxes = document.getElementsByName('checkbox'); //ByName make an array of checkbox elements in html document  
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        checkboxes[i].checked = maincheckbox.checked;
+    }
+}
+
+//__________________function for every single element________________
+
+
+
+
+/* "housemade" but actually useless
 function checkboxall() {
     if (document.querySelector('#maincheckbox').checked) {
         for (var i = 0; i < numeroitem; i++)
@@ -29,15 +66,10 @@ function checkboxall() {
     }
 }*/
 
-function checkboxall(maincheckbox) {
-    var checkboxes = document.getElementsByName('checkbox'); //ByName make an array of checkbox elements in html document  
-    for (var i = 0, n = checkboxes.length; i < n; i++) {
-        checkboxes[i].checked = maincheckbox.checked;
-    }
-}
-
-
-//https://www.w3schools.com/jsref/prop_html_id.asp
-//https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
-//https://www.w3schools.com/js/js_htmldom_html.asp
-//https://getbootstrap.com/docs/5.0/components/list-group/#checkboxes-and-radios
+/*  some important stuff
+https://www.w3schools.com/js/js_classes.asp
+https://www.w3schools.com/jsref/prop_html_id.asp
+https://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
+https://www.w3schools.com/js/js_htmldom_html.asp
+https://getbootstrap.com/docs/5.0/components/list-group/#checkboxes-and-radios
+*/
